c951bf1b6acb9988777e9bd731106a3d2711f2b8
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2025-03-28 14:13:12 -0600

    Adding more paths to utils

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,6 @@ import git
 
 __git_repo = git.Repo(".", search_parent_directories=True)
 GIT_ROOT = __git_repo.git.rev_parse("--show-toplevel")
+
+MODELS_PATH = f"{GIT_ROOT}/models"
+DATASETS_PATH = f"{GIT_ROOT}/datasets"
